944ac29e570cde0ced4fbb4be1cab3edef3127f5
Aleks Pawlak <aleks.pawlak@code.berlin>
2021-03-05 23:22:30 +0100

    added logging

--- color_check/website.py
+++ color_check/website.py
@@ -1,7 +1,7 @@
+import logging
 from flask import Flask
 from flask import render_template
 from flask import request
-import logging
 from color_check.controllers.get_color_code import get_color_code
 app = Flask(__name__)
 
@@ -13,15 +13,7 @@ def index():
 
 @app.route('/color', methods=['POST'])
 def show_color():
-    # When the user submits the form at /, the contents of the form
-    # will be send to this route, and whatever code you write here will
-    # be run by your server. In order to render a new page for your user,
-    # you will need to do a few things:
-    # - extract the data submitted by the user
-    # - check if the color exists in our list, return the hex code if it does
-    # - render a new page which shows a square of that color and its name
-    # - if the color doesn't exist, give the user a useful error message.
-    # - create a log.txt file which records (logs) the user requests.
+
     color_name = request.form['color']
     color_hex_code = get_color_code(color_name)
     if color_hex_code == "error":
@@ -35,6 +27,10 @@ def show_color():
         return render_template('color.html', page_title="Your Color",
                                your_color=your_color,
                                color_hex_code=color_hex_code)
+
+    logging.basicConfig(filename='tmp/log.txt', filemode='w',
+                        level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
+    logging.debug(f'User request: {color_name}')
 
 
 if __name__ == "__main__":
